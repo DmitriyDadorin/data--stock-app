37f2462625947ae2e3929e284865f3119c1a33f5
Doomer77 <newzibkoff1984@gmail.com>
2019-01-23 17:17:17 +0400

    add header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import ButtonAdd from './Components/button_add';
-import ButtonWriteOff from './Components/button_write_off';
-import InputSearch from './Components/input_search';
-import ButtonInDb from './Components/button_in_db';
+import Header from './Router/header';
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="ButtonSection">
-          <ButtonInDb />
-          <ButtonAdd />
-          <ButtonWriteOff />
-          <InputSearch />
-        </div>
+        <Header />
       </div>
     );
   }
--- src/Components/button_write_off/ButtonWriteOff.js
+++ src/Components/button_write_off/ButtonWriteOff.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
+import './button_Write_Off.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const ButtonWriteOff = () => {
     return(
         <div>
-        <button className='btn btn-danger'>Списание товара</button>
+            <button className='btn btn-danger'>Списание товара</button>
         </div>
     )
 }
