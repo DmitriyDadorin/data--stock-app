2ec60021e14b1bd1c27948feb209bcfb02113f8a
Doomer77 <newzibkoff1984@gmail.com>
2019-01-24 15:11:28 +0400

    error NavPannel line 14

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
-import Header from './Router/header';
-
-
+import NavPannel from './Router/NavPannel'; 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <NavPannel/>
       </div>
     );
   }
---
+++ src/Router/NavPannel.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { HashRouter } from 'react-router-dom';
+import ButtonAdd from '../../Components/button_add';
+import ButtonWriteOff from '../../Components/button_write_off';
+import InputSearch from '../../Components/input_search';
+import ButtonInDb from '../../Components/button_in_db';
+
+const {
+    HashRouter,
+    Switch,
+    Route,
+    Link
+  } = ReactRouterDOM
+
+
+const Header = () => {
+    return (
+        <div className="ButtonSection">
+          <Link to='/databasepannel'><ButtonInDb /></Link>
+          <Link to='/addtobasepannel'><ButtonAdd /></Link>
+          <Link to='/removedatabasepannel'><ButtonWriteOff /></Link>
+          <InputSearch />
+        </div>
+    )
+}
+
+const Main = () => {
+    return (
+        <div className="App">
+            <Switch>
+                <Route exact path='/databasepannel' component={DataBasePannel}/> 
+                <Route exact path='/addtobasepannel' component={AddToBasePannel}/>
+                <Route exact path='/removedatabasepannel' component={RemoveDataBasePannel}/>
+            </Switch>
+        </div>
+    )
+}
+
+const DataBasePannel = () => {
+    return(
+        <div>
+            <h1>Page1</h1>
+        </div>
+    )
+}
+
+const AddToBasePannel = () => {
+    return(
+        <div>
+            <h1>Page2</h1>
+        </div>
+    )
+}
+
+const RemoveDataBasePannel = () => {
+    return(
+        <div>
+            <h1>Page3</h1>
+        </div>
+    )
+}
+
+const NavPannel = () => {
+    return(
+        <div>
+            <Header />
+            <Main />
+        </div>
+    )
+}
+
+const NavPannel = () => {
+    return(
+        <div>
+            <HashRouter>
+                <NavPannel />
+            </HashRouter>
+        </div>
+    )
+}
+
+export default NavPannel;
