1489a5085e4e03427c28e420f5a37046cba13261
Insayder <dm7rin@gmail.com>
2019-01-30 20:48:12 +0300

    firebase modified hz

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import firebase from 'firebase';
 
 const config = {
@@ -13,16 +12,3 @@ const config = {
 
   export default firebase;
   export const database = firebase.database();
-=======
-import firebase from 'firebase'
-
-const config = {
-    apiKey: "AIzaSyAuCt9ThQKgVXn9ZD515NPVhNEOHt2ONS4",
-    authDomain: "data-stock-app.firebaseapp.com",
-    databaseURL: "https://data-stock-app.firebaseio.com",
-    projectId: "data-stock-app",
-    storageBucket: "data-stock-app.appspot.com",
-    messagingSenderId: "692773553782"
-  };
-  firebase.initializeApp(config);
->>>>>>> 4c74d89b41170d219051a001fcaa346ac38dbd34
